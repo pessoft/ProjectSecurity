6b88dce9da9c1c6bc1ab4eaca700483709859368
Pess <sharpmsil@gmail.com>
2016-10-04 22:51:15 +0300

    Helper Converter data

---
+++ Security/Security/Helpers/FrequencyHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+namespace Security.Helpers
+{
+    public static class FrequencyHelper
+    {
+        public static Chart ConverToChart(this IFrequency source)
+        {
+            var result = new Chart();
+            result.Series[0].ChartType = SeriesChartType.Line;
+            result.ChartAreas[0].AxisX.Minimum = 0;
+            result.ChartAreas[0].AxisX.Interval = 1;
+            result.ChartAreas[0].AxisX.IsLabelAutoFit = false;
+            result.ChartAreas[0].AxisX.LabelAutoFitMinFontSize = 9;
+            result.ChartAreas[0].AxisX.TextOrientation = TextOrientation.Horizontal;
+
+            int numb = 0;
+            foreach (var let in source.GetLetterFreq().OrderBy(p=>p.Key))
+            {
+                result.Series[0].Points.AddXY(++numb, let.Value);
+                result.ChartAreas[0].AxisX.CustomLabels.Add(numb, numb + 0.5, let.Key);
+            }
+            return result;
+        }
+
+    }
+}
